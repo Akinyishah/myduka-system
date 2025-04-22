505e27bbed1b5a6f68efaefe25d9b60999d92176
Shalyne Akinyi <51117887+Akinyishah@users.noreply.github.com>
2025-04-22 21:00:15 +0300

    changes made

--- database.py
+++ database.py
@@ -17,18 +17,37 @@ time=datetime.now() # creating time as at now
 #for product in products: #On loop you must always print the iteration e.g (i or product)
 #for sale in sales: #for loop if you want your data to look presentable and readable.
 
-# def fetching_products():
-#  cur.execute('select * from products;')
-#  products=cur.fetchall()
-#  for product in products:  
-#   # print(product)
+def fetching_products():
+  cur.execute('select * from products;')
+  products=cur.fetchall()
+  return products
+#   print(product)
 
-# def fetch_sales():
-#  cur.execute('select * from sales;')
-#  sales=cur.fetchall()
-#  for sale in sales:
-#   # print(sale)
+def fetch_sales():
+ cur.execute('select * from sales;')
+ sales=cur.fetchall()
+ return products
+# print(sale)
 
+# fetch_sales() 
+# fetching_products()  
+
+#TASK REVIEW- USED FOR ANY TABLE OF MY CHOICE 
+# Modify your select and insert functions to be able 
+# select and insert data from any table.
+# Hint: let your functions take parameters(table,data)-USE A FORMATTED STRING cause its a variable uses F string 
+
+def fetch_data(table):
+  cur.execute(f"select * from {table} ;")
+  data=cur.fetchall()
+  return data
+
+products=fetch_data('products')
+sales=fetch_data('sales')
+#print("products from fetch data func:\n", products)
+#print("sales from fetch data func:\n", sales)
+
+# INSERTING PRODUCTS 
 def insert_products():
      cur.execute("insert into products(name,buying_price,selling_price,stock_quantity)values('Apple cider Vinegar',1210.50,1579.00,80)")
      conn.commit
@@ -38,36 +57,40 @@ def insert_sales():
      cur.execute(f"insert into sales(pid,quantity,created_at)values(1,110,'{time}')")# when passing variable as a string use formated string f''
      conn.commit
      return "sales made"
-    
-# fetch_sales() 
-# fetching_products()  
 
-#TASK REVIEW- USED FOR ANY TABLE OF MY CHOICE 
-# Modify your select and insert functions to be able 
-# select and insert data from any table.
-# Hint: let your functions take parameters(table,data)-USE A FORMATTED STRING
-
-def fetch_data(table):
-  cur.execute(f"select * from {table} ;")
-  data=cur.fetchall()
-  return data
-
-products=fetch_data('products')
-sales=fetch_data('sales')
-print("products from fetch data func:\n", products)
-#print("sales from fetch data func:\n", sales)
-
-
-#INSERT PRODUCTS -METHOD 1 TAKES VALUES AS PARAMETERS;
+#INSERT PRODUCTS -METHOD 1 TAKES VALUES AS PARAMETERS AND USES placeholders which acts as temporary value;
+#NUMBER OF PLACEHOLDERS HAVE TO MATCH NUMBER OF COLUMNS
 def insert_products(values):
-  #use placeholders which acts as temporary value
   insert_products="insert into products(name,buying_price,selling_price,stock_quantity)values(%s,%s,%s,%s)"
   cur.execute(insert_products,values)
   conn.commit()
 
-#provide the product values
-product_values=('potatoes',1000,1150,50)
-insert_products(product_values)
+#provide the product values that you want to add.
+product_values1=('potatoes',1000,1150,50)
+product_values2=('blenders',9600,11150,30)
+# insert_products(product_values1)
+# insert_products(product_values2)
+# products=fetch_data('products') # CALL IT AFTER INSERT SO AS TO SEE IF ITS ADDED
+#print("fetch data after modification func.\n",products)
+
+  #INSERT PRODUCTS -METHOD 2 still TAKES VALUES AS PARAMETERS BUT DOES NOT USE PLACEHOLDERS USES A FORMATED STRING
+  #INSTEAD WE USE PLACEHOLDERS WITH{VALUES} PARAMETER IN A FOMARTED STRING 
+
+def insert_products_method_2(values):
+  insert = f"insert into products(name,buying_price,selling_price,stock_quantity)values{values}"
+  cur.execute(insert)
+  conn.commit()
+
+product1=("laptop",24500,32600,70) #should be outside the def function.After conn.commit remove indentation
+insert_products_method_2(product1)
 products=fetch_data('products')
-print("fetch data after modification func.\n",products)
+#print("fetching prods using method2:\n",products)
+
+
+
+
+
+
+
+
 
