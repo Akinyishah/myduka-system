7120e61d231cdf2e04687d96d484d73bfd840608
Shalyne Akinyi <51117887+Akinyishah@users.noreply.github.com>
2025-04-25 16:49:12 +0300

    change

--- main.py
+++ main.py
@@ -12,8 +12,23 @@ app=Flask(__name__)
 #Functions MUST HAVE unique NAME
 
 @app.route('/')          #func 1
-def home():              #func 2   
-    return render_template("index.html")
+def home():               #func 2  
+    user={"name":"Akinyi","location":"Nairobi"}    
+    num=[1,2,3,4,5]   
+    return render_template("index.html",data=user,num=num)#declaring variable for variable e.g data=name
+
+# TASK.
+# Create 2 files products.html and sales.html 
+# render them using flask render_template
+
+@app.route('/products')
+def products():
+    return render_template("products.html")
+
+
+@app.route('/sales')
+def sales():
+    return render_template("sales.html")
 
 #running an application one has to tell FLASK 
 app.run(debug=True)
